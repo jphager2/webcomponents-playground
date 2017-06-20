062d1a2bb3f3ca50d09d9239140cf9279f0f6155
jphager2 <jphager2@gmail.com>
2017-06-20 13:06:07 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var fs = require('fs');
+
+const PORT = '3000';
+
+const MIMES = {
+  'html': 'text/html',
+  'js': 'text/javascript',
+  'css': 'text/css',
+};
+
+var mimeType = function(filename) {
+  var match = filename.match(/\.([^\.]+)$/);
+  var extension = match && match[1];
+
+  var mime = MIMES[extension] || MIMES['html'];
+
+  return `${mime}; charset=utf-8`;
+};
+
+var finalize = function(response, code, headers, data) {
+  response.writeHead(code, headers);
+  response.end(data);
+};
+
+var absolutePath = function(path) {
+  return '/Users/jph/projects/node/webcomponents' + path;
+};
+
+var server = http.createServer(function(request, response) {
+  var path = request.url;
+  var mime = mimeType(path);
+  var filename;
+
+  console.log(`${request.method}: ${path}`);
+
+  if (path === '/') {
+    path = '/index.html';
+  }
+
+  filename = absolutePath(path);
+
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      finalize(response, 404);
+      return;
+    }
+
+    finalize(response, 200, {'ContentType': mime}, data);
+  });
+});
+
+server.listen(PORT);
+
+console.log(`Server listening on ${PORT}`);
